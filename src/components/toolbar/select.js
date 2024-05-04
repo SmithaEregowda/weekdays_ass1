74d4783f4233165d859fdaca3cff8d8ce87bb6cc
SmithaEregowda <smithahe@gmail.com>
2024-05-04 14:40:45 +0530

    filters added

--- src/components/toolbar/select.js
+++ src/components/toolbar/select.js
@@ -1,20 +1,29 @@
-import React from 'react'
-import { MenuItem, Select } from '@mui/material'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 import './toolbar.css'
 const UISelect = ({value,handleChange,label,options}) => {
   return (
-    <Select
-    labelId={label}
-    id={label}
-    value={value}
-    onChange={handleChange}
-    label={label}
-    fullWidth={true}
-  >
-    {options?.map((item)=>(
-         <MenuItem value={item}>{item}</MenuItem>
-    ))}
-  </Select>
+  <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">{label}</InputLabel>
+                <Select
+            labelId={label}
+            id={label}
+            value={value&&value}
+            onChange={handleChange}
+            label={label}
+            fullWidth={true}
+        >
+            {options?.map((item)=>item)?.map((item)=>(
+                <MenuItem value={item}>{item}</MenuItem>
+            ))}
+        </Select>
+      </FormControl>
+    </Box>
   )
 }
 
